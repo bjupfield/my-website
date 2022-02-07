8f88ad3f7e705fb930d3e32d93f6e58dc12a1c7a
Bennett <bjupfield@gmail.com>
2022-02-06 18:04:58 -0600

    waves

--- client/src/Components/DivEditor.js
+++ client/src/Components/DivEditor.js
@@ -76,6 +76,157 @@ function DivEditor(){
         setPoints(line)
         setPathTrue(line.join(" "))
     }
+    function cutTool(){
+        const test = pathD.split(" ");
+        const line = [test[1], test[2], test[4], test[5]];
+        const slope = (parseFloat(line[3]) - parseFloat(line[1])) / (parseFloat(line[2]) - parseFloat(line[0]));
+        const lineEquat = [slope, parseFloat(line[1]) - (slope * parseFloat(line[0]))]
+        const lines = pathTrue.split("L").map(point=>{
+            const b = point.split(" ").filter(factor =>{
+                if(factor === ""){
+                    return false;
+                }
+                return true
+            })
+            return b
+        })
+        const pointIntersect = lines.map((point, ind) =>{
+            const secondPoint = lines[ind + 1];
+            let intersectLine = [];
+            if(point[point.length - 1] === "z"){
+                switch(point.length){
+                    case 3:
+                        intersectLine = [point[0], point[1], lines[0][1], lines[0][2]] // make cover cases for if m happens in other places
+                }
+            }
+            else{
+                switch(point.length){
+                    case 3:
+                        if(point[0] === "M"){
+                        switch(secondPoint.length){
+                            case 3:
+                                if(secondPoint[0] === "M"){
+                                    return [false, 0, 0]
+                                }
+                                else{
+                                    intersectLine = [point[1], point[2], secondPoint[0], secondPoint[1]]
+                                }
+                                break;
+                            case 2:
+                                intersectLine = [point[1], point[2], secondPoint[0], secondPoint[1]]
+                                break;
+                        }
+                        }
+                        else{    
+                        console.log("shouldnt happen");
+                        }
+                        break;
+                    case 2:
+                    switch(secondPoint.length){
+                        case 3:
+                            if(secondPoint[0] === "M"){
+                                return [false, 0, 0]
+                            }
+                            else{
+                                intersectLine = [point[0], point[1], secondPoint[0], secondPoint[1]]
+                            }
+                            break;
+                        case 2:
+                            intersectLine = [point[0], point[1], secondPoint[0], secondPoint[1]]
+                            break;
+                    }   
+                    break;
+                }
+            }
+            switch(intersectLine.length){
+                case 4: 
+                    const sloped = (parseFloat(intersectLine[3]) - parseFloat(intersectLine[1])) / (parseFloat(intersectLine[2]) - parseFloat(intersectLine[0]));
+                    const lineEquat2 = [sloped, parseFloat(intersectLine[1]) - (parseFloat(intersectLine[0]) * sloped)]
+                    let x;
+                    if(lineEquat[0] === Infinity || lineEquat[0] ===  -Infinity){
+                        if(lineEquat2[0] === Infinity || lineEquat2[0] === -Infinity){
+                            return [false, 0, 0]
+                        }
+                        else{
+                            x = parseFloat(line[0])
+                        }
+                    }
+                    else{
+                        if(lineEquat2[0] === Infinity || lineEquat2[0] === -Infinity){
+                            x = parseFloat(intersectLine[0])
+                        }
+                        else{
+                            x = (lineEquat[1] - lineEquat2[1]) / (lineEquat2[0] - lineEquat[0]);
+                        }
+                    }
+                    const newLine = line.map(li=>parseFloat(li));
+                    const newIntersect = intersectLine.map(li=>parseFloat(li));
+                    if(x >= (newIntersect[0] <= newIntersect[2] ? newIntersect[0] : newIntersect[2]) && x <= (newIntersect[0] >= newIntersect[2] ? newIntersect[0] : newIntersect[2])){
+                        if(x >= (newLine[0] <= newLine[2] ? newLine[0] : newLine[2]) && x <= (newLine[0] >= newLine[2] ? newLine[0] : newLine[2])){
+                            return [true, x, slope * x + lineEquat[1]]
+                        }
+                        else{
+                            return [false, 0, 0]
+                        }
+                    }
+                    else{
+                        return [false, 0, 0]
+                    }
+                    break;
+            }
+        });
+        addPoints(pointIntersect)
+    }
+    function addPoints(points){
+        console.log(points)
+        let sanityCount = 0;
+        let newTruePath = pathTrue.split("L").map(path=>path.split(" ").filter(b=>b !== ""));
+        points.forEach((arr , ind)=>{
+            if(arr[0] === true){
+                const previousPoint = newTruePath[ind + sanityCount];
+                switch(previousPoint.length){
+                    case 3:
+                        if(previousPoint[0] === "M"){
+                            newTruePath.splice(ind + sanityCount + 1, 0, [arr[1].toString(), arr[2].toString()])
+                        }
+                        else{
+                            newTruePath.splice(ind + sanityCount, 1, [previousPoint[0], previousPoint[1]], [arr[1], arr[2], previousPoint[2]])
+                        }
+                        break;
+                    case 2:
+                        newTruePath.splice(ind + sanityCount + 1, 0, [arr[1].toString(), arr[2].toString()])
+                        break;
+                }
+                ++sanityCount;
+            }
+            else{
+            }
+        })
+        newTruePath = newTruePath.map((path, ind)=>{
+            switch(path.length){
+                case 3:
+                    if(path[0] === "M"){
+                    }
+                    else{
+                        switch(newTruePath[ind - 1].length){
+                            case 2:
+                                path = ["L", path[0], path[1], path[2]]
+                                break;
+                        }
+                    }
+                    break;
+                case 2:
+                    path = ["L", path[0], path[1]]
+                    break;
+            }
+            return path.join(" ")
+        })
+        console.log(pathTrue)
+        console.log(newTruePath)
+        setPoints(newTruePath);
+        setPathTrue(newTruePath.join(" "));
+
+    }
     let e = 1;
     useEffect(()=>{
         const d = document.querySelector("#absolute");
@@ -107,6 +258,9 @@ function DivEditor(){
         setMouseDownPos([x, y]);
     }
     function onMouseUpCall(){
+        if(pathD !== ""){
+            cutTool()
+        }
         setMouseDownPos([])
         setClientBorder([])
         setPathD("")
@@ -144,10 +298,10 @@ function DivEditor(){
             {points.map((point, ind)=>{
                 const f = point.split(" ");
                 if(f[2] === "z"){
-                    return <div className="adjustor" id={`${ind}`} style={{position:"absolute", top:`${parseFloat(f[1]) - 5}px`, left:`${parseFloat(f[0]) - 5}px`, height:"10px", width:"10px"}}
+                    return <div className="adjustor" id={`${ind}`} style={{position:"absolute", top:`${parseFloat(f[1])}px`, left:`${parseFloat(f[0])}px`, height:"10px", width:"10px"}}
                     onMouseDown={(e)=>onMouseDownAdjustor(e)} ><svg viewBox="0, 0, 10, 10"><circle cx={5} cy={5} r={5} fill="red"></circle></svg></div>
                 }
-                return <div className="adjustor" id={`${ind}`} style={{position:"absolute", top:`${parseFloat(f[2]) - 5}px`, left:`${parseFloat(f[1]) - 5}px`, height:"10px", width:"10px"}}
+                return <div className="adjustor" id={`${ind}`} style={{position:"absolute", top:`${parseFloat(f[2]) + 1}px`, left:`${parseFloat(f[1]) + 1}px`, height:"10px", width:"10px"}}
                 onMouseDown={(e)=>onMouseDownAdjustor(e)} ><svg viewBox="0, 0, 10, 10"><circle cx={5} cy={5} r={5} fill="red"></circle></svg></div>
             })}
         </div>
